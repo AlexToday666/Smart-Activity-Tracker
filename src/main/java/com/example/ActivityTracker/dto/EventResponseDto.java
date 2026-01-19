80ec4339f87419af8092e81e7a3d646c68f3d3b1
Alex Today <ernestkudakaev6@mail.ru>
2026-01-19 11:50:15 +0300

    add EventMapper && add dto

--- src/main/java/com/example/ActivityTracker/dto/EventResponseDto.java
+++ src/main/java/com/example/ActivityTracker/dto/EventResponseDto.java
@@ -1,4 +1,51 @@
 package com.example.ActivityTracker.dto;
 
+import java.time.Instant;
+
 public class EventResponseDto {
+    private Long id;
+    private String userId;
+    private String eventType;
+    private String metadata;
+    private Instant eventTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(String metadata) {
+        this.metadata = metadata;
+    }
+
+    public Instant getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(Instant eventTime) {
+        this.eventTime = eventTime;
+    }
 }
--- src/main/java/com/example/ActivityTracker/mapper/EventMapper.java
+++ src/main/java/com/example/ActivityTracker/mapper/EventMapper.java
@@ -1,4 +1,42 @@
 package com.example.ActivityTracker.mapper;
 
+
+import com.example.ActivityTracker.dto.EventResponseDto;
+import com.example.ActivityTracker.model.Event;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EventMapper {
+
+    // DTO to Entity
+    public Event toEntity(EventResponseDto dto) {
+
+        if (dto == null) {
+            return null;
+        }
+
+        Event event = new Event();
+        event.setUserId(dto.getUserId());
+        event.setEventType(dto.getEventType());
+        event.setMetadata(dto.getMetadata());
+        event.setEventTime(dto.getEventTime());
+
+        return event;
+    }
+
+    // Entity to DTO
+    public EventResponseDto toDto(Event event) {
+        if (event == null) {
+            return null;
+        }
+        EventResponseDto dto = new EventResponseDto();
+        event.setId(event.getId());
+        event.setUserId(event.getUserId());
+        event.setEventType(event.getEventType());
+        event.setMetadata(event.getMetadata());
+        event.setEventTime(event.getEventTime());
+        event.setCreatedAt(event.getCreatedAt());
+
+        return dto;
+    }
 }
