2bee3e1025c77f21c3e8134136b054b9f8da58d5
AlexToday111 <ernestkudakaev6@mail.ru>
2026-01-22 18:35:01 +0300

    add SwaggerEndpointsDto

--- src/main/java/com/example/ActivityTracker/dto/EventRequestDto.java
+++ src/main/java/com/example/ActivityTracker/dto/EventRequestDto.java
@@ -1,14 +1,22 @@
 package com.example.ActivityTracker.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import java.time.Instant;
 
 public class EventRequestDto {
+    @Schema(description = "Идентификатор пользователя", example = "user-123", required = true)
     @NotBlank(message = "userId is required")
     private String userId;
+
+    @Schema(description = "Тип события", example = "click", required = true)
     @NotBlank(message = "eventType is required")
     private String eventType;
+
+    @Schema(description = "Произвольные метаданные в формате JSON (строка)", example = "{\"button\":\"buy\"}")
     private String metadata;
+    @Schema(description = "Время события (ISO-8601). Если не указано, устанавливается текущее время",
+            example = "2026-01-21T12:00:00Z")
     private Instant eventTime;
 
     public String getUserId() {
--- src/main/java/com/example/ActivityTracker/dto/EventResponseDto.java
+++ src/main/java/com/example/ActivityTracker/dto/EventResponseDto.java
@@ -1,12 +1,24 @@
 package com.example.ActivityTracker.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.Instant;
 
 public class EventResponseDto {
+
+    @Schema(description = "Идентификатор события", example = "1")
     private Long id;
+
+    @Schema(description = "Идентификатор пользователя", example = "user-123")
     private String userId;
+
+    @Schema(description = "Тип события", example = "click")
     private String eventType;
+
+    @Schema(description = "Метаданные события", example = "{\"button\":\"buy\"}")
     private String metadata;
+
+    @Schema(description = "Время события", example = "2026-01-21T12:00:00Z")
     private Instant eventTime;
 
     public Long getId() {
